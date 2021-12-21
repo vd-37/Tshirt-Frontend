3a4efce7be9915296d9f2430c3f8d57575d7a89a
vd-37 <vinayakadayanand007@gmail.com>
2021-12-21 19:52:13 +0530

    first commit

---
+++ build/precache-manifest.d9bc28b3b1426d711d70161068ad371e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee503af38b785f09d16b3e12d55aa763",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c43f4d9932861fb0e4e",
+    "url": "/static/css/main.890f977b.chunk.css"
+  },
+  {
+    "revision": "4c1f9e42fa48abfdcdfc",
+    "url": "/static/js/2.9776d67f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9776d67f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c43f4d9932861fb0e4e",
+    "url": "/static/js/main.8f19db1c.chunk.js"
+  },
+  {
+    "revision": "72017dee5443dfc32ffd",
+    "url": "/static/js/runtime-main.dc49cf19.js"
+  }
+]);
