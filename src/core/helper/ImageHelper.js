c27d1dd91e3b236328ebc2fa7b0e6edb91286490
Vinayaka D <62375990+vd-37@users.noreply.github.com>
2022-12-08 23:04:04 +0530

    Image links updated

--- src/core/helper/ImageHelper.js
+++ src/core/helper/ImageHelper.js
@@ -23,40 +23,40 @@ const ImageHelper = ({ product }) => {
   var imageurl = "https://raw.githubusercontent.com/vd-37/TshirtImages/main/wrogn4.webp";
   switch ((String)(product.name)) {
     case 'Levis1':
-      imageurl = "https://raw.githubusercontent.com/vd-37/TshirtImages/main/levis1.webp";
+      imageurl = "https://drive.google.com/file/d/1T7gG2eKbt2S-gY73TBp197Vqm5A82EeO/view?usp=share_link";
       break;
     case 'Levis2':
-      imageurl = "https://raw.githubusercontent.com/vd-37/TshirtImages/main/levis2.webp";
+      imageurl = "https://drive.google.com/file/d/1B5FIap2QMyFTZWpC7JTh3U8AMIlNIvZZ/view?usp=share_link";
       break;
     case 'Levis3':
-      imageurl = "https://raw.githubusercontent.com/vd-37/TshirtImages/main/levis3.webp";
+      imageurl = "https://drive.google.com/file/d/1BqMxhocjCH8j4bpgvHqCkTMY48wxsZVr/view?usp=share_link";
       break;
     case 'Levis4':
-      imageurl = "https://raw.githubusercontent.com/vd-37/TshirtImages/main/levis4.webp";
+      imageurl = "https://drive.google.com/file/d/133Rx764A27lIHYwHGiNl3V7egV3bKLSG/view?usp=share_link";
       break;
     case 'Puma1':
-      imageurl = "https://raw.githubusercontent.com/vd-37/TshirtImages/main/puma1.webp";
+      imageurl = "https://drive.google.com/file/d/17FjdVMorh2N4jJQqu4exyISeluRuX4ne/view?usp=share_link";
       break;
     case 'Puma2':
-      imageurl = "https://raw.githubusercontent.com/vd-37/TshirtImages/main/puma2.webp";
+      imageurl = "https://drive.google.com/file/d/14JkoNKvb7X_qKEDTVIUmrmFXtICpzIxH/view?usp=share_link";
       break;
     case 'Puma3':
-      imageurl = "https://raw.githubusercontent.com/vd-37/TshirtImages/main/puma3.webp";
+      imageurl = "https://drive.google.com/file/d/1sdQ6LJqgGQPuRdAjmcc1nvgnOmGwobs8/view?usp=share_link";
       break;
     case 'Puma4':
-      imageurl = "https://raw.githubusercontent.com/vd-37/TshirtImages/main/puma4.webp";
+      imageurl = "https://drive.google.com/file/d/1PTcbJuSQ-nyw5Qn1tejTx6hlO2Vk6Qri/view?usp=share_link";
       break;
     case 'Wrogn1':
-      imageurl = "https://raw.githubusercontent.com/vd-37/TshirtImages/main/wrogn1.webp";
+      imageurl = "https://drive.google.com/file/d/1lllnKYy0FYoB_gUeRvneDAsydW7rmRVZ/view?usp=share_link";
       break;
     case 'Wrogn2':
-      imageurl = "https://raw.githubusercontent.com/vd-37/TshirtImages/main/wrogn2.webp";
+      imageurl = "https://drive.google.com/file/d/1FRdsmmKt-SlAfL6FUfU1nmuvFhxuO-Sy/view?usp=share_link";
       break;
     case 'Wrogn3':
-      imageurl = "https://raw.githubusercontent.com/vd-37/TshirtImages/main/wrogn3.webp";
+      imageurl = "https://drive.google.com/file/d/12V3_3YTsv2ib-NAvu3TB8Z46ONDwneEf/view?usp=share_link";
       break;
     case 'Wrogn4':
-      imageurl = "https://raw.githubusercontent.com/vd-37/TshirtImages/main/wrogn4.webp";
+      imageurl = "https://drive.google.com/file/d/19_gjKsmpuq8lAzktEdeCc8FobGJsgxIX/view?usp=share_link";
       break;
     default:
       break;
